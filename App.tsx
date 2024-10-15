929a3aa007d7fd69753e7cd536a4b02287a63590
JyotiDahal123 <jyotirajdahal6@gmail.com>
2024-10-15 14:06:04 -0500

    Boilar Plate code

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello, Welcome To My React Native Mobile Application.</Text>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent:"center",
+    alignItems:"center"
   },
+  boldText:{
+    fontWeight:"bold"
+  }
+  
 });
